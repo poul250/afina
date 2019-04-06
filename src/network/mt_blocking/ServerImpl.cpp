a7603c7b8260c8c0e361ca077339765592155c33
Kulis Pavel <poul250@yandex.ru>
2019-04-06 17:17:58 +0300

    fix stop function

--- src/network/mt_blocking/ServerImpl.cpp
+++ src/network/mt_blocking/ServerImpl.cpp
@@ -80,6 +80,7 @@ void ServerImpl::Start(uint16_t port, uint32_t n_accept, uint32_t n_workers) {
 // See Server.h
 void ServerImpl::Stop() {
     running.store(false);
+    this->join();
     shutdown(_server_socket, SHUT_RDWR);
 }
 
